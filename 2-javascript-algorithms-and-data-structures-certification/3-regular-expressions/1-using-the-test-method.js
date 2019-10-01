4fcc909194f929dcd2e65b34d783e974a4ef4d46
Marcelo Vilela <marcelo.vilela.s@gmail.com>
2019-10-01 15:21:01 -0300

    3-regular-expressions/

---
+++ 2-javascript-algorithms-and-data-structures-certification/3-regular-expressions/1-using-the-test-method.js
@@ -0,0 +1,4 @@
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+//let result = myRegex; // Change this line
+let result = myRegex.test(myString); // Change this line
---
+++ 2-javascript-algorithms-and-data-structures-certification/3-regular-expressions/22-restrict-possible-usernames.js
@@ -0,0 +1,3 @@
+let username = "JackOfAllTrades";
+let userCheck = /[a-zA-Z][a-zA-Z]+[0-9]*/; // Change this line
+let result = userCheck.test(username);
