4436c63baeebaafd7c5f05b40be51d633929414b
Marcelo Vilela <marcelo.vilela.s@gmail.com>
2019-10-02 16:03:57 -0300

    4-debugging/

---
+++ 2-javascript-algorithms-and-data-structures-certification/4-debugging/4-catch-misspelled-variable-and-function-names.js
@@ -0,0 +1,5 @@
+let receivables = 10;
+let payables = 8;
+//let n_etWorkingCapital = r_ecievables - p_ayable;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
